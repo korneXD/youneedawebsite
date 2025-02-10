1108b8401f853a2fd04947cf8255f52aec2a8421
korneXD <korcika2005@gmail.com>
2025-02-10 22:10:36 +0100

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,17 +1,12 @@
 import fs from "fs";
 import path from "path";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"; // Default URL for local development
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 async function getLastModifiedDate(filePath) {
   filePath = path.join(process.cwd(), filePath);
-  try {
-    const stats = await fs.promises.stat(filePath);
-    return stats.mtime;
-  } catch (error) {
-    console.error(`Error getting last modified date for ${filePath}:`, error);
-    return new Date(); // Return current date if error occurs
-  }
+  const stats = await fs.promises.stat(filePath);
+  return stats.mtime;
 }
 
 export default async function sitemap() {
@@ -21,9 +16,8 @@ export default async function sitemap() {
       const lastModified = await getLastModifiedDate(
         `src/app/${pageRoute}/page.js`,
       );
-      const page = pageRoute.replace(/\/?\(.*?\)/g, "").replace(/^\/+/, "");
       return {
-        url: `${BASE_URL}/${page}`,
+        url: `${BASE_URL}`,
         lastModified,
       };
     }),
